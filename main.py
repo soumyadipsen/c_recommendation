4afed14705e7ef8754b7ff20a271e7f5a4821997
Soumyadip Sen <soumya.sen.98@gmail.com>
2020-06-11 18:13:23 +0530

    commit5

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+import csv
 import pandas as pd
 import sys
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
 
 
+prod="product.csv"
 
 
 
@@ -32,15 +33,29 @@ def recommend():
 	
 	n = request.args.get('n')
 	n=int(n)
+	f=[]
+	r=[]
 	output=customer_recomendation(n)
 	if(type(output)==type('string')):
-		return render_template('index.html', prediction_text='Customer Not Found')
+		return render_template('index.html', pt1='Customer Not Found')
 	else:
 		o=""
 		o=o+output[0]
 		output=o.split("|")
 		print(output)
-		return render_template('index.html', prediction_text='Recommended Products are: {}'.format(output))
+		pt=[]
+		xyz=[]
+		with open(prod,'r') as csvfile:
+			csvreader=csv.reader(csvfile)
+			f=next(csvreader)
+			for rows in csvreader:
+				xyz.append(rows)
+		for ii in range(10):
+			for row in xyz:
+				if (output[ii] == row[0]):
+					pt.append(row[1])
+		print(pt)
+		return render_template('index.html',pt=pt)
 
 
 if __name__ == "__main__":
